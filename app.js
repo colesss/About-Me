1b2fe7d23fc66da701e8f8b21eec6123fb9fa1d5
Cole Allen <decolesss@gmail.com>
2017-05-25 20:19:20 -0700

    about-me base code with developer notes

--- app.js
+++ app.js
@@ -1,9 +1,68 @@
 'use strict';
 
-var confirmed = comfirm ('Are you ready to rock and roll!?');
+//confirm asking user if they are ready to play
+var confirmed = confirm ('');
 
+//if/else statement responding to previous command block depending on whether user pressed ok or not
 if(confirmed === true){
-  console.log('The user is ready!');
+  console.log('');
 } else {
-  console.log('The user said they were not ready...but too bad!')
+  console.log('')
+}
+
+//prompt asking for the user's name, console.log logging name entered into var user
+var user = prompt ('');
+  console.log('' + user);
+
+//alert greeting user and informing user about coming questions
+alert('' + user + '');
+
+//prompt for question1, telling user that this is a y/n question and then asking question about developer
+var answer1 = prompt (user + '');
+
+//if/else statement with responses to whether the question1 was answered correctly, condition needs a response that matches prompt format
+if (answer1.toUpperCase () === '') {
+  alert('');
+} else {
+  alert('');
+}
+
+//prompt for question2, telling user that this is a y/n question and then asking question about developer
+var answer2 = prompt (user + '');
+
+//if/else statement with responses to whether the question2 was answered correctly, condition needs a response that matches prompt format
+if (answer2.toUpperCase () === '') {
+  alert('');
+} else {
+  alert('');
+}
+
+//prompt for question3, telling user that this is a y/n question and then asking question about developer
+var answer3 = prompt (user + '');
+
+//if/else statement with responses to whether the question3 was answered correctly, condition needs a response that matches prompt format
+if (answer3.toUpperCase () === '') {
+  alert('');
+} else {
+  alert('');
+}
+
+//prompt for question4, telling user that this is a y/n question and then asking question about developer
+var answer4 = prompt (user + '');
+
+//if/else statement with responses to whether the question4 was answered correctly, condition needs a response that matches prompt format
+if (answer4.toUpperCase () === '') {
+  alert('');
+} else {
+  alert('');
+}
+
+//prompt for question5, telling user that this is a y/n question and then asking question about developer
+var answer5 = prompt (user + '');
+
+//if/else statement with responses to whether the question5 was answered correctly, condition needs a response that matches prompt format
+if (answer5.toUpperCase () === '') {
+  alert('');
+} else {
+  alert('');
 }
