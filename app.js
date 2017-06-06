61d98be803eb8781a613380b9ea4ca2a90d4a619
Cole Allen <decolesss@gmail.com>
2017-06-05 18:20:55 -0700

    lab 3 updates

--- app.js
+++ app.js
@@ -1,91 +1,119 @@
 'use strict';
 
-//confirm asking user if they are ready to play
+//confirmed: asks user if they are ready to play
 var confirmed = confirm ('Are you sure you want to know more about Cole?');
 
 //if/else statement responding to previous command block depending on whether user pressed ok or not
 if(confirmed){
   alert('You do!?! D\'awwww');
 } else {
-  alert('Cancel?!? That but is close enough to ok...');
+  alert('Cancel?!? That is close enough to ok...');
 }
 
-//prompt asking for the user's name, console.log logging name entered into var user
 var user = prompt ('Wait a minute...who is asking?');
   console.log('The user\'s name is ' + user);
+  alert('Ohhhhhh, heya ' + user + '! Let\'s answer five yes and no questions about Cole and see how many you get right, yeah?');
 
-//alert greeting user and informing user about coming questions
-alert('Ohhhhhh, heya ' + user + '! Let\'s answer some yes and no questions about Cole and see how many you get right, yeah?');
+//correct: tracks correct answers
+var correct = 0;
 
-//prompt for question1, telling user that this is a y/n question and then asking question about developer
-
-
-//if/else statement with responses to whether the question1 was answered correctly, condition needs a response that matches prompt format
+//yes or no quessing questions
 function question1() {
-  var answer1 = prompt (user + ', does Cole love tattoos? Make sure you answer with Yes or No!');
-  if (answer1.toUpperCase() == 'YES') {
-  alert('Hmmmm, that was pretty obvious wasn\'t it? Next question!');
-} else if (answer1.toUpperCase() == 'NO') {
-  alert('No! Really? Ummm, did you see their tats? Errr...well I suppose they are covered most of the time since it is super cold in the classrooms.');
-} else {
-  alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  var question1 = prompt (user + ', does Cole love tattoos? Make sure you answer with Yes or No!');
+  if (question1.toUpperCase() == 'YES') {
+    alert('Hmmmm, that was pretty obvious wasn\'t it? Next question!');
+    console.log('User\'s choice was ' + question1 + '.');
+    correct++;
+    console.log('Correct=' + '' + correct);
+  } else if (question1.toUpperCase() == 'NO') {
+    alert('No! Really? Ummm, did you see their tats? Errr...well I suppose they are covered most of the time since it is super cold in the classrooms.');
+  } else {
+    alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  }
 }
-}
-//prompt for question2, telling user that this is a y/n question and then asking question about developer
 
-
-//if/else statement with responses to whether the question2 was answered correctly, condition needs a response that matches prompt format
 function question2() {
-  var answer2 = prompt (user + ', did Cole turn 20 earlier this month?');
-  if (answer2.toUpperCase () == 'YES') {
-  alert('HAHAHAHAHA, you flatterer you!!!');
-} else if (answer2.toUpperCase() == 'NO') {
-  alert('*snorts* That is correct! Bartenders stopped carding Cole when they started drinking gin on the rocks. Hey!!! No judgement, it\'s a quick dry martini with no fuss.');
-} else {
-  alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  var question2 = prompt (user + ', did Cole turn 20 earlier this month?');
+  if (question2.toUpperCase () == 'YES') {
+    alert('HAHAHAHAHA, you flatterer you!!!');
+  } else if (question2.toUpperCase() == 'NO') {
+    alert('*snorts* That is correct! Bartenders stopped carding Cole when they started drinking gin on the rocks. Hey!!! No judgement, it\'s a quick dry martini with no fuss.');
+      console.log('User\'s choice was ' + question2 + '.');
+      correct++;
+        console.log('Correct=' + '' + correct);
+  } else {
+    alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  }
 }
-}
-//prompt for question3, telling user that this is a y/n question and then asking question about developer
 
-
-//if/else statement with responses to whether the question3 was answered correctly, condition needs a response that matches prompt format
 function question3(){
-  var answer3 = prompt (user + ', does Cole have a dog?');
-  if (answer3.toUpperCase () === 'YES') {
-  alert('They certainly do! His name is Cid and he loves to cuddle, as pitties typically do with their humans.');
-} else if (answer3.toUpperCase() == 'NO') {
-  alert('Ahh, so you haven\'t noticed the dog hairs. Good!');
-} else {
-  alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  var question3 = prompt (user + ', does Cole have a dog?');
+  if (question3.toUpperCase () === 'YES') {
+    alert('They certainly do! His name is Cid and he loves to cuddle, as pitties typically do with their humans.');
+      console.log('User\'s choice was ' + question1 + '.');
+      correct++;
+      console.log('Correct=' + '' + correct);
+  } else if (question3.toUpperCase() == 'NO') {
+    alert('Ahh, so you haven\'t noticed the dog hairs. Good!');
+  } else {
+    alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  }
 }
-}
-//prompt for question4, telling user that this is a y/n question and then asking question about developer
 
-
-//if/else statement with responses to whether the question4 was answered correctly, condition needs a response that matches prompt format
 function question4(){
-  var answer4 = prompt (user + ', does Cole know a bit of sign language?');
-  if (answer4.toUpperCase () === 'YES') {
-  alert('That is correct! They learned a bit a couple years ago and can\'t wait to learn more.');
-} else if (answer4.toUpperCase() == 'NO') {
-  alert('Actually, Cole is the one in front of class looking at Rowen to confirm the letters used in those tricky short cut keys.');
-} else {
-  alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  var question4 = prompt (user + ', does Cole know a bit of sign language?');
+  if (question4.toUpperCase () === 'YES') {
+    alert('That is correct! They learned a bit a couple years ago and can\'t wait to learn more.');
+      console.log('User\'s choice was ' + question1 + '.');
+      correct++;
+      console.log('Correct=' + '' + correct);
+  } else if (question4.toUpperCase() == 'NO') {
+    alert('Actually, Cole is the one in front of class looking at Rowen to confirm the letters used in those tricky short cut keys.');
+  } else {
+    alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  }
 }
-}
-//prompt for question5, telling user that this is a y/n question and then asking question about developer
 
-
-//if/else statement with responses to whether the question5 was answered correctly, condition needs a response that matches prompt format
 function question5(){
-  var answer5 = prompt ('Last question, ' + user + '! Is Cole\'s favorite beer an IPA?');
-  if (answer5.toUpperCase () === 'YES') {
-  alert('Wow, you really do know Cole. No beer is too hoppy!');
-} else if (answer5.toUpperCase() == 'NO') {
-  alert('Pssshhh, terrible! In Cole\'s world, there is no better beer!');
-} else {
-  alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  var question5 = prompt ('Last question, ' + user + '! Is Cole\'s favorite beer an IPA?');
+  if (question5.toUpperCase () === 'YES') {
+    alert('Wow, you really do know Cole. No beer is too hoppy!');
+    console.log('User\'s choice was ' + question1 + '.');
+    correct++;
+    console.log('Correct=' + '' + correct);
+  } else if (question5.toUpperCase() == 'NO') {
+    alert('Pssshhh, terrible! In Cole\'s world, there is no better beer!');
+  } else {
+    alert('Errmmmhh, sorry. I did not understand your response. I\'m gonna count that as a no.');
+  }
 }
+
+var answer6 = false;
+var guesses = 5;
+var guess = true;
+
+function question6(){
+  var question6 = prompt('What is my favorite number. You have ' + guesses + ' guesses left.');
+
+  for(var i = 0; i < 5; i++){
+    if (guesses === 0) {
+      alert('Oh dear...no more guesses!');
+      guess = false;
+    }
+    if (answer > 5) {
+      alert('Nope! That was too high!');
+      guess--;
+    }
+    if (answer < 5) {
+      alert('Nope! That was too low.');
+      guess--;
+    }
+    if (guesstion6 === 5) {
+      alert('Yup! My favorite number is 5');
+      guess = false;
+      correct++;
+    }
+  }
 }
 
 question1();
@@ -93,3 +121,4 @@ question2();
 question3();
 question4();
 question5();
+question6();
